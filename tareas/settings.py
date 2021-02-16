48a4a9059dd23b533b1e5f9bbaf94eeac2462d1a
Eduardo Agreda <checkups_bb@protonmail.com>
2021-02-15 18:51:59 -0600

    configuración base de datos con postgres

--- tareas/settings.py
+++ tareas/settings.py
@@ -86,9 +86,17 @@ WSGI_APPLICATION = 'tareas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
